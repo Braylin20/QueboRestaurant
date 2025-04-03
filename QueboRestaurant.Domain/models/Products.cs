67b8b8810a60a183aa4b441442743bfc9e9d2160
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-04-02 23:43:38 -0400

    Add Add Products model

---
+++ QueboRestaurant.Domain/models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueboRestaurant.Domain.models;
+public class Products
+{
+    [Key]
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public double Cost { get; set; }
+    public int Cuantity { get; set; }
+    public double Itbis { get; set; }
+    [ForeignKey("Categories")]
+    public int CategoryId { get; set; }
+    public Categories Categories { get; set; }
+
+}
