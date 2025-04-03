6bd6af80647089fc41621444621fab732f5361e1
Eliezer Terrero <eliezerterrero12@gmail.com>
2025-04-03 14:46:43 -0400

    Add ICategoriesService interface for category management
    This commit introduces the `ICategoriesService` interface in the `QueboRestaurant.Abstractions.Interfaces` namespace. It defines five asynchronous methods: `GetAllAsync`, `AddAsync`, `PutAsync`, `DeleteAsync`, and `GetByIdAsync` for managing categories.

---
+++ QueboRestaurant.Abstractions/Interfaces/ICategoriesService.cs
@@ -0,0 +1,19 @@
+﻿using QueboRestaurant.Domain.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueboRestaurant.Abstractions.Interfaces
+{
+    public interface ICategoriesService
+    {
+        public Task<List<Categories>> GetAllAsync();
+        public Task<bool> AddAsync(Categories category);
+        public Task<Categories> PutAsync(Categories category);
+        public Task<Categories> DeleteAsync(int CategoryId);
+        public Task<Categories> GetByIdAsync(int CategoryId);
+
+    }
+}
