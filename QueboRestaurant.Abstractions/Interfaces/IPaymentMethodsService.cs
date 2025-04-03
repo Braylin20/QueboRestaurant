fa1cc7ec72cd2a3ebf6e875fcc54df31afba7fce
Eliezer Terrero <eliezerterrero12@gmail.com>
2025-04-03 14:47:24 -0400

    Add IPaymentMethodsService interface for payment management
    Introduces the `IPaymentMethodsService` interface in the
    `QueboRestaurant.Abstractions.Interfaces` namespace. This
    interface includes five asynchronous methods:
    `GetAllAsync`, `AddAsync`, `PutAsync`, `DeleteAsync`,
    and `GetByIdAsync`.

---
+++ QueboRestaurant.Abstractions/Interfaces/IPaymentMethodsService.cs
@@ -0,0 +1,18 @@
+﻿using QueboRestaurant.Domain.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueboRestaurant.Abstractions.Interfaces
+{
+    public interface IPaymentMethodsService
+    {
+        public Task<List<PaymentMethods>> GetAllAsync();
+        public Task<bool> AddAsync(PaymentMethods paymentMethod);
+        public Task<PaymentMethods> PutAsync(PaymentMethods paymentMethod);
+        public Task<PaymentMethods> DeleteAsync(int paymentMethodId);
+        public Task<PaymentMethods> GetByIdAsync(int paymentMethodId);
+    }
+}
