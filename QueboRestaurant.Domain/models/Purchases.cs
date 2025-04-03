f601722e11ec1a6b6bc2a1c4d3c1d99633068a40
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-04-02 23:43:45 -0400

    Add Pruchases model

---
+++ QueboRestaurant.Domain/models/Purchases.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueboRestaurant.Domain.models;
+
+public class Purchases
+{
+    [Key]
+    public int PurchasesId { get; set; }
+    [ForeignKey("PaymentMethodId")]
+    public int PaymentMethodId { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+}
