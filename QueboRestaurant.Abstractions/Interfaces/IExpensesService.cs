ff846f2294bce44865027994d2a7166bc2559ba3
Eliezer Terrero <eliezerterrero12@gmail.com>
2025-04-03 14:47:12 -0400

    Add IExpensesService interface for expense management
    Introduces the `IExpensesService` interface in the
    `QueboRestaurant.Abstractions.Interfaces` namespace.
    This interface includes five asynchronous methods:
    `GetAllAsync`, `AddAsync`, `PutAsync`, `DeleteAsync`,
    and `GetByIdAsync`. Also adds necessary using directives
    for relevant namespaces.

---
+++ QueboRestaurant.Abstractions/Interfaces/IExpensesService.cs
@@ -0,0 +1,18 @@
+﻿using QueboRestaurant.Domain.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueboRestaurant.Abstractions.Interfaces
+{
+    public interface IExpensesService
+    {
+        public Task<List<Expenses>> GetAllAsync();
+        public Task<bool> AddAsync(Expenses expense);
+        public Task<Expenses> PutAsync(Expenses expense);
+        public Task<Expenses> DeleteAsync(int expenseId);
+        public Task<Expenses> GetByIdAsync(int expenseId);
+    }
+}
