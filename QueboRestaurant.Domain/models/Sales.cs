b4dcb71735d395f437d0c7b1cce33b120ea92023
Braylin20 <braylin_vasquez@ucne.edu.do>
2025-04-02 23:44:15 -0400

    Add Sales model

---
+++ QueboRestaurant.Domain/models/Sales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueboRestaurant.Domain.models;
+
+public class Sales
+{
+    [Key]
+    public int SaleId { get; set; }
+    [ForeignKey("ClientId")]
+    public int ClientId { get; set; }
+    [ForeignKey("PaymentMethodId")]
+    public int PaymentMethodId { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+
+    [ForeignKey("SaleId")]
+    public ICollection<SalesDetails> SalesDetails { get; set; } = new List<SalesDetails>();
+}
