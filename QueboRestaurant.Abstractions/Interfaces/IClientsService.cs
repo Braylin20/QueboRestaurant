b9f1f42e0118669cfc3cd95f4091dc67a0e91901
Eliezer Terrero <eliezerterrero12@gmail.com>
2025-04-03 14:47:02 -0400

    Add IClientsService interface for client management
    Introduces the `IClientsService` interface in the
    `QueboRestaurant.Abstractions.Interfaces` namespace.
    This interface includes five asynchronous methods:
    `GetAllAsync`, `AddAsync`, `PutAsync`, `DeleteAsync`,
    and `GetByIdAsync` for managing client data.

---
+++ QueboRestaurant.Abstractions/Interfaces/IClientsService.cs
@@ -0,0 +1,18 @@
+﻿using QueboRestaurant.Domain.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueboRestaurant.Abstractions.Interfaces
+{
+    public interface IClientsService
+    {
+        public Task<List<Clients>> GetAllAsync();
+        public Task<bool> AddAsync(Clients client);
+        public Task<Clients> PutAsync(Clients client);
+        public Task<Clients> DeleteAsync(int clientId);
+        public Task<Clients> GetByIdAsync(int clientId);
+    }
+}
